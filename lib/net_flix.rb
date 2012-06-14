9fb1c09ff42df1d16eaa1866f564d1b59b3f7ae6
Amos Wenger <amos@official.fm>
2012-06-14 04:25:52 +0200

    Accept logger

--- lib/net_flix.rb
+++ lib/net_flix.rb
@@ -2,23 +2,17 @@ module Netflix
 
   class << self
 	  
-    def configure(yml_path)
+    def configure(yml_path, logger = nil)
       @credentials ||= Netflix::Credentials.from_file(yml_path)
+      @logger = logger
     end
 
     def credentials
       @credentials
     end
   
-    def logfile
-      File.join( File.dirname(__FILE__), '..', 'log', 'netflix.log' )
-    end
-
     def create_logger
-      logdir = File.join( File.dirname(__FILE__), '..', 'log' )
-      Dir.mkdir(logdir) unless File.exists? logdir
-
-      Logger.new( logfile ) 
+      Logger.new(STDOUT) 
     end
 
     def logger
